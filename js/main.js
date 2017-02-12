39631c9ad13221b13e5769c3a9e047abf38d1307
Rameez Khan <Rameez@Rameezs-MacBook-Pro.local>
2017-02-12 22:12:27 +0000

    list shows up now with predefined coffee

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ function initMap() {
             new Location('Coffee To Go', 53.797950, -1.549262, '4bf6c03013aed13a6823eaf7'),
         ],
 
-        query: ko.observable(''),
+        query: ko.observable('coffee'),
     };
     // Search function to filter throught the locationsModel location list based on the name of the location
     locationsModel.search = ko.dependentObservable(function() {
